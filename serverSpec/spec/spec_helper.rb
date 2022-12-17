1d87de4f33fb2a8baf4adea6f7b00b3371349161
Yuta_Ushizima <contact@yuta-u.com>
2022-12-17 12:12:36 +0900

    fix: serverspecでログインするuserをec2-userに固定

--- serverSpec/spec/spec_helper.rb
+++ serverSpec/spec/spec_helper.rb
@@ -18,7 +18,7 @@ host = ENV['TARGET_HOST']
 
 options = Net::SSH::Config.for(host)
 
-options[:user] ||= Etc.getlogin
+options[:user] ||= 'ec2-user'
 
 set :host,        options[:host_name] || host
 set :ssh_options, options
